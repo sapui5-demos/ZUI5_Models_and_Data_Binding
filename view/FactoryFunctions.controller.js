034433d89e50534f20abdeae790f7e2c49103c03
kicker123 <chriskne+00@gmail.com>
2015-10-03 10:28:53 +0000

    alignment with presentation - part 2

--- view/FactoryFunctions.controller.js
+++ view/FactoryFunctions.controller.js
@@ -1,11 +1,10 @@
 sap.ui.controller("sap.training.view.FactoryFunctions", {
 
-	/**
-	 * Called when a controller is instantiated and its View controls (if available) are already created.
-	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-	 * @memberOf sap.training.view.FactoryFunctions
-	 */
 	onInit: function() {
+
+		var oModel = new sap.ui.model.json.JSONModel();
+		oModel.loadData("json/data_3.json");
+		this.getView().setModel(oModel);
 
 		var oPanel = this.getView().byId("myPanel");
 
@@ -23,47 +22,27 @@ sap.ui.controller("sap.training.view.FactoryFunctions", {
 					var oCell = new sap.ui.commons.layout.MatrixLayoutCell();
 
 					var sPath = "{" + prop + "}";
-					
+
 					switch (typeof oObject[prop]) {
 						case "string":
-							oControl = new sap.ui.commons.TextView({ text: sPath }); break;
+							oControl = new sap.ui.commons.TextView({
+								text: sPath
+							});
+							break;
 						case "boolean":
-							oControl = new sap.ui.commons.CheckBox({ checked: sPath }); break;
+							oControl = new sap.ui.commons.CheckBox({
+								checked: sPath
+							});
+							break;
 					}
 
 					oCell.addContent(oControl);
 					oRow.addCell(oCell);
 				}
-				
+
 				oLayout.addRow(oRow);
 				return oLayout;
 			});
-	},
-
-	/**
-	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-	 * (NOT before the first rendering! onInit() is used for that one!).
-	 * @memberOf sap.training.view.FactoryFunctions
-	 */
-	//	onBeforeRendering: function() {
-	//
-	//	},
-
-	/**
-	 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-	 * This hook is the same one that SAPUI5 controls get after being rendered.
-	 * @memberOf sap.training.view.FactoryFunctions
-	 */
-	//	onAfterRendering: function() {
-	//
-	//	},
-
-	/**
-	 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-	 * @memberOf sap.training.view.FactoryFunctions
-	 */
-	//	onExit: function() {
-	//
-	//	}
+	}
 
 });
--- view/Main2.controller.js
+++ view/Main2.controller.js
@@ -1,38 +1,11 @@
 sap.ui.controller("sap.training.view.Main2", {
 
-	/**
-	 * Called when a controller is instantiated and its View controls (if available) are already created.
-	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-	 * @memberOf training.sap.view.Main
-	 */
-    //  onInit: function() {
-    //
- 	//  },
+	onInit: function() {
 
-	/**
-	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-	 * (NOT before the first rendering! onInit() is used for that one!).
-	 * @memberOf training.sap.view.Main
-	 */
-	//	onBeforeRendering: function() {
-	//
-	//	},
+		var oModel = new sap.ui.model.json.JSONModel();
+		oModel.loadData("json/data_2.json");
+		this.getView().setModel(oModel);
 
-	/**
-	 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-	 * This hook is the same one that SAPUI5 controls get after being rendered.
-	 * @memberOf training.sap.view.Main
-	 */
-	//	onAfterRendering: function() {
-	//
-	//	},
-
-	/**
-	 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-	 * @memberOf training.sap.view.Main
-	 */
-	//	onExit: function() {
-	//
-	//	}
+	}
 
 });
